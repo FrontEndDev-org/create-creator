5dad597855dc68ae17934a207d04288e85809b84
☁️淡然 <hi@ydr.me>
2025-01-15 01:10:07 +0800

    refactor: 将 createCLI 函数改为异步函数

--- templates/default/src/index.ts
+++ templates/default/src/index.ts
@@ -2,7 +2,7 @@ import path from 'node:path';
 import { createCreator } from 'create-creator';
 import { pkgDescription, pkgName, pkgVersion } from './const';
 
-export function createCLI() {
+export async function createCLI() {
   return createCreator({
     projectPath: process.argv[2],
     templatesRoot: path.join(__dirname, '../templates'),
