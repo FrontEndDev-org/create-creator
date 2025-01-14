10f3c5356244eda2b487e0e2efab709b5e1468be
☁️淡然 <hi@ydr.me>
2025-01-15 00:13:58 +0800

    chore: 工具函数

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,7 @@ import fs from 'node:fs';
 export function isDirectory(p: string): boolean {
   return fs.existsSync(p) && fs.statSync(p).isDirectory();
 }
+
+export function isFile(p: string): boolean {
+  return fs.existsSync(p) && fs.statSync(p).isFile();
+}
