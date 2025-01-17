20f5d29f21a4910b3fd24282ab6c81ddd69be2a7
☁️淡然 <hi@ydr.me>
2025-01-18 01:01:12 +0800

    refactor(MiddleWare): 重构中间件执行逻辑
    - 移除了 MiddleWareOptions 中的泛型参数
    - 新增 setPaths 方法动态设置路径
    - 修改 at 方法返回值类型
    - 优化了测试用例，移除了不必要的 mock 函数

--- src/MiddleWare.ts
+++ src/MiddleWare.ts
@@ -2,21 +2,20 @@ import path from 'node:path';
 import { normalizePath } from './utils';
 
 export type MiddleWareCallback<I extends unknown[], O> = (...args: I) => O | Promise<O>;
-export type MiddleWareOptions<I extends unknown[], O> = {
+export type MiddleWareOptions = {
   cwd: string;
   paths: string[];
-  executor: (output: O) => unknown | Promise<unknown>;
 };
 
 export class MiddleWare<I extends unknown[], O> {
-  files: string[];
-  fileSet = new Set<string>();
-  constructor(readonly options: MiddleWareOptions<I, O>) {
-    this.files = options.paths.map((pth) => normalizePath(path.join(this.options.cwd, pth)));
+  #files: string[] = [];
+  #fileSet = new Set<string>();
+  constructor(private readonly options: MiddleWareOptions) {
+    this.setPaths(options.paths);
+  }
 
-    if (this.files.length === 0) {
-      throw new Error('No files found');
-    }
+  setPaths(paths: string[]) {
+    this.#files = paths.map((pth) => normalizePath(path.join(this.options.cwd, pth)));
   }
 
   #hooks: {
@@ -29,15 +28,15 @@ export class MiddleWare<I extends unknown[], O> {
       files: (Array.isArray(pth) ? pth : [pth]).map((p) => {
         const file = normalizePath(path.join(this.options.cwd, p));
 
-        if (!this.files.includes(file)) {
+        if (!this.#files.includes(file)) {
           throw new Error(`File ${p} not found`);
         }
 
-        if (this.fileSet.has(file)) {
+        if (this.#fileSet.has(file)) {
           throw new Error(`File ${p} already matched`);
         }
 
-        this.fileSet.add(file);
+        this.#fileSet.add(file);
 
         return file;
       }),
@@ -46,18 +45,18 @@ export class MiddleWare<I extends unknown[], O> {
     return this;
   }
 
-  async at(pth: string, ...inputs: I) {
+  async at(pth: string, ...inputs: I): Promise<O | undefined> {
     const file = normalizePath(path.join(this.options.cwd, pth));
 
-    if (!this.files.includes(file)) {
+    if (!this.#files.includes(file)) {
       throw new Error(`File ${pth} not found`);
     }
 
     const hooks = this.#hooks.filter((hook) => hook.files.includes(file));
 
-    for (const hook of hooks) {
-      const output = await hook.callback(...inputs);
-      await this.options.executor(output);
-    }
+    if (hooks.length === 0) return;
+    if (hooks.length === 1) return hooks[0].callback(...inputs);
+
+    throw new Error(`Multiple hooks found for file ${pth}`);
   }
 }
--- test/MiddleWare.test.ts
+++ test/MiddleWare.test.ts
@@ -3,16 +3,11 @@ import { expect, it, vi } from 'vitest';
 import { MiddleWare } from '../src/MiddleWare';
 
 it('单个文件', async () => {
-  const fn1 = vi.fn();
   const fn2 = vi.fn();
   const cwd = '/path/to';
   const mw = new MiddleWare<[number, string], boolean>({
     cwd,
     paths: ['file1.ts'],
-    executor: (output) => {
-      expect(output).toBeTypeOf('boolean');
-      fn1();
-    },
   });
 
   mw.is('file1.ts', (a, b) => {
@@ -21,8 +16,7 @@ it('单个文件', async () => {
     fn2();
     return true;
   });
-  await mw.at('./file1.ts', 1, '2');
-  expect(fn1).toBeCalledTimes(1);
+  expect(await mw.at('./file1.ts', 1, '2')).toBeTypeOf('boolean');
   expect(fn2).toBeCalledTimes(1);
 });
 
@@ -33,10 +27,6 @@ it('多个文件', async () => {
   const mw = new MiddleWare<[number, string], boolean>({
     paths: ['file1.ts', 'x/y/file2.js'],
     cwd,
-    executor: (output) => {
-      expect(output).toBeTypeOf('boolean');
-      fn1();
-    },
   });
 
   mw.is(['file1.ts', 'x\\y/file2.js'], (a, b) => {
@@ -46,58 +36,46 @@ it('多个文件', async () => {
     return true;
   });
 
-  await mw.at('file1.ts', 1, '2');
-  await mw.at('x/y/file2.js', 1, '2');
+  expect(await mw.at('file1.ts', 1, '2')).toBe(true);
+  expect(await mw.at('x/y/file2.js', 1, '2')).toBe(true);
 
-  expect(fn1).toBeCalledTimes(2);
   expect(fn2).toBeCalledTimes(2);
 });
 
 it('空 paths 应该报错', async () => {
-  const fn = vi.fn();
   const cwd = '/path/to';
+  const mw = new MiddleWare<[], boolean>({
+    cwd,
+    paths: [],
+  });
 
-  expect(
-    () =>
-      new MiddleWare<[number, string], boolean>({
-        cwd,
-        paths: [],
-        executor: () => {
-          fn();
-        },
-      }),
-  ).toThrowError('No files found');
+  expect(() => mw.is('file1.ts', () => true)).toThrowError('File file1.ts not found');
+  await expect(mw.at('file2.ts')).rejects.toThrowError('File file2.ts not found');
+
+  mw.setPaths(['file1.ts']);
+  mw.is('file1.ts', () => true);
+  await expect(mw.at('file1.ts')).resolves.toBe(true);
 });
 
 it('不匹配文件应该报错', async () => {
-  const fn = vi.fn();
   const cwd = '/path/to';
   const mw = new MiddleWare<[number, string], boolean>({
     cwd,
     paths: ['file1.ts'],
-    executor: () => {
-      fn();
-    },
   });
 
   expect(() => mw.is('non-existent.ts', () => true)).toThrowError('File non-existent.ts not found');
-  expect(fn).toBeCalledTimes(0);
 });
 
 it('重复判断的文件应该报错', async () => {
-  const fn = vi.fn();
   const cwd = '/path/to';
   const mw = new MiddleWare<[number, string], boolean>({
     cwd,
     paths: ['file1.ts'],
-    executor: () => {
-      fn();
-    },
   });
 
   mw.is('file1.ts', () => true);
   expect(() => mw.is('file1.ts', () => true)).toThrowError('File file1.ts already matched');
-  expect(fn).toBeCalledTimes(0);
 });
 
 it('不存在的文件应该报错', async () => {
@@ -106,11 +84,7 @@ it('不存在的文件应该报错', async () => {
   const mw = new MiddleWare<[number, string], boolean>({
     cwd,
     paths: ['file1.ts'],
-    executor: () => {
-      fn();
-    },
   });
 
   await expect(mw.at('non-existent.ts', 1, '2')).rejects.toThrowError('File non-existent.ts not found');
-  expect(fn).toBeCalledTimes(0);
 });
