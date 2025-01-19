ed44d9b997f9559a62ac535508b490c0d19ff8a9
☁️淡然 <hi@ydr.me>
2025-01-19 21:26:49 +0800

    feat: 更新 2.0 模板

--- templates/default/src/index.ts
+++ templates/default/src/index.ts
@@ -1,20 +1,24 @@
 import path from 'node:path';
 import process from 'node:process';
-import { createCreator } from 'create-creator';
+import { Creator } from 'create-creator';
 import { pkgDescription, pkgName, pkgVersion } from './const';
 
 export async function createCLI() {
-  return createCreator({
+  const creator = new Creator({
     projectPath: process.argv[2],
     templatesRoot: path.join(__dirname, '../templates'),
-    onStart({ prompts, colors }) {
-      prompts.intro(colors.bold(colors.bgCyan(` ${pkgName}@${pkgVersion} `)));
-      prompts.log.info(pkgDescription);
-    },
-    onEnd({ prompts, colors, projectPath }) {
-      prompts.log.success('The project has been created successfully!');
-      prompts.log.success(`${colors.bold(colors.greenBright(`cd ${projectPath}`))} to start your coding journey`);
-      prompts.outro('🎉🎉🎉');
-    },
   });
+
+  creator.on('start', ({ prompts, colors }) => {
+    prompts.intro(colors.bold(colors.bgCyan(` ${pkgName}@${pkgVersion} `)));
+    prompts.log.info(pkgDescription);
+  });
+
+  creator.on('end', ({ prompts, colors, projectPath }) => {
+    prompts.log.success('The project has been created successfully!');
+    prompts.log.success(`${colors.bold(colors.greenBright(`cd ${projectPath}`))} to start your coding journey`);
+    prompts.outro('🎉🎉🎉');
+  });
+
+  await creator.create();
 }
