796283c838d5a822f1bb4b491a19ce9d34e30448
☁️淡然 <hi@ydr.me>
2025-01-21 15:32:39 +0800

    refactor(src): 修改 CreatorError 默认退出码为 1
    修改 CreatorError 类的构造函数，将默认的 exitCode 参数值从 0 改为 1。这样可以确保在抛出错误时，默认情况下会使用非零退出码，更符合错误处理的预期。

--- src/CreatorError.ts
+++ src/CreatorError.ts
@@ -2,7 +2,7 @@ import { prompts } from './prompts';
 
 export class CreatorError extends Error {
   exitCode: number;
-  constructor(message: string, exitCode = 0) {
+  constructor(message: string, exitCode = 1) {
     super(message);
     this.name = 'CreateError';
     this.exitCode = exitCode;
