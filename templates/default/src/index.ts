0093c0643b2c0a3944c5800bc7de7de2ffe2d887
☁️淡然 <hi@ydr.me>
2025-01-21 19:44:23 +0800

    build(default): 完善 ESLint 配置并简化 CLI 创建逻辑
    - 在 package.json 中添加 tsc --noEmit 命令，确保 TypeScript 文件在 lint 时被检查
    - 移除 createCLI 函数中冗余的模板逻辑，简化项目创建流程

--- templates/default/src/index.ts
+++ templates/default/src/index.ts
@@ -1,23 +1,11 @@
 import path from 'node:path/posix';
 import process from 'node:process';
-import { Creator, prompts, colors } from 'create-creator';
-import { pkgDescription, pkgName, pkgVersion } from './const';
+import { Creator } from 'create-creator';
 
 export async function createCLI() {
   const creator = new Creator({
     projectPath: process.argv[2],
     templatesRoot: path.join(__dirname, '../templates'),
-  });
-
-  creator.on('before', () => {
-    prompts.intro(colors.bold(colors.bgCyan(` ${pkgName}@${pkgVersion} `)));
-    prompts.log.info(pkgDescription);
-  });
-
-  creator.on('end', ({ projectPath }) => {
-    prompts.log.success('The project has been created successfully!');
-    prompts.log.success(`${colors.bold(colors.greenBright(`cd ${projectPath}`))} to start your coding journey`);
-    prompts.outro('🎉🎉🎉');
   });
 
   // create 方法不会抛错，不必捕获
